7e56f06240677eec33c13676e549bcb6f47d7bd9
Mustafa Yücel <mustafayucel.cs@gmail.com>
2025-05-06 16:13:57 +0300

    Removed GLib async command spawning

---
+++ src/methods.ts
@@ -0,0 +1,26 @@
+// Copyright (C) 2025 Mustafa Yücel <mustafayucel.cs@gmail.com>
+
+// This file is part of Statistig.
+
+// Statistig is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Statistig is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Statistig. If not, see <https://www.gnu.org/licenses/>.
+
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+export class StatistigMethods {
+    openPreferences: (() => void);
+
+    constructor(ext: Extension) {
+        this.openPreferences = () => ext.openPreferences();
+    }
+}
